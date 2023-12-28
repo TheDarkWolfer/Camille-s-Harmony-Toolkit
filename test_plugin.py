85aee7c4efc4fb704a90999fde28e916f0a0d15f
Camille.Is_Me <97792990+TheDarkWolfer@users.noreply.github.com>
2023-12-28 23:50:26 +0100

    Update test_plugin.py
    Made the test_plugin better <3

--- test_plugin.py
+++ test_plugin.py
@@ -1,26 +1,43 @@
 PLUGIN_METADATA = {
-    'name': 'Uppercase Converter',
+    'name': 'Demo Plugin',
     'version': '1.0',
     'author': 'Camille.Is_Me',
-    'description': 'A demo plugin to help users understand how plugins work',
-    'dependencies':[] # Write down your script's dependencies here
+    'description': 'A demo plugin for the plugin system, and for the users so that they have a template to go off of.',
+    'dependencies' : ['datetime'] #Fill out with your plugin's dependencies
 }
 
 def info():
     """
-    Returns information about the plugin.
+    Returns information about the plugin. Better not touch this function, as
+    it is used to provide information about this plugin to the main program, and
+    tampering with this function might cause issues.
+
+    Have fun <3
     """
     return PLUGIN_METADATA
 
 def register_args(parser):
     """
-    Register arguments for the demo plugin.
+    Register arguments for the plugin.
     """
-    parser.add_argument('--me', type=str, help='Function for the demo plugin')
+
+    #Here we register all the arguments our plugin needs to function.
+    parser.add_argument('--me', type=str, help="Simple function to print the file's name.")
+    parser.add_argument('--date', type=str, help="Simple function to print the current date.")
 
 def execute(args):
     """
-    Run the code in here
+    Execute the whole plugin.
     """
+
+    import datetime
+    #Here we import the libraries we need and
+    #have listed above in the plugin's metadata
+
+    #And here is where we execute the code depending on 
+    #what arguments were provided.
     if args.me:
-        print(f"This plugin is name {__file__}")
+        print(__file__)
+
+    if args.date:
+        print(datetime.datetime.now())
